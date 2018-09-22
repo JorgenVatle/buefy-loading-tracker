44ad5866ddf59a7bd867fc0c007d833f46d99eed
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-22 23:07:21 +0300

    Add base state tracking mixin.

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+const defaultKey = 'global';
+
+export default {
+
+    data() {
+        return {
+            loading: {
+                global: false,
+            }
+        }
+    },
+
+    methods: {
+        /**
+         * Checks if the given tracker is loading.
+         *
+         * @param trackerName
+         */
+        isLoading(trackerName: string = defaultKey) {
+            return this.loading[trackerName];
+        },
+
+        /**
+         * Starts loading for the given tracker.
+         *
+         * @param trackerName
+         */
+        startLoading(trackerName: string = defaultKey) {
+            return this.$set(this.loading, trackerName, true);
+        },
+
+        /**
+         * Stops the loading state for the given tracker.
+         *
+         * @param trackerName
+         */
+        stopLoading(trackerName: string = defaultKey) {
+            return this.$set(this.loading, trackerName, false);
+        },
+
+        /**
+         * Bulma loading class for the given tracker.
+         *
+         * @param trackerName
+         */
+        loadingClass(trackerName: string = defaultKey) {
+            return {
+                'is-loading': this.isLoading(trackerName),
+            }
+        }
+    }
+
+}
