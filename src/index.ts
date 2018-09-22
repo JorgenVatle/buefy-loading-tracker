0e1a50c0eef1ee241bcbf55f2e08bab3e5dbfbb5
Jørgen Vatle <Jorgen@WcAServices.net>
2018-09-22 23:51:45 +0300

    Use class-style Vue mixin.

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,54 @@
 import Vue from 'vue';
+import { Mixin } from "vue-mixin-decorator";
+
 const defaultKey = 'global';
+const defaultState: any = {};
+defaultState[defaultKey] = false;
 
-export default Vue.extend({
+@Mixin
+export default class BuefyLoadingTracker extends Vue {
 
-    data(): { loading: any } {
+    /**
+     * Loading states
+     */
+    loading = defaultState;
+
+    /**
+     * Checks if the given tracker is loading.
+     *
+     * @param trackerName
+     */
+    isLoading(trackerName: string = defaultKey) {
+        return this.loading[trackerName];
+    }
+
+    /**
+     * Starts loading for the given tracker.
+     *
+     * @param trackerName
+     */
+    startLoading(trackerName: string = defaultKey) {
+        return this.$set(this.loading, trackerName, true);
+    }
+
+    /**
+     * Stops the loading state for the given tracker.
+     *
+     * @param trackerName
+     */
+    stopLoading(trackerName: string = defaultKey) {
+        return this.$set(this.loading, trackerName, false);
+    }
+
+    /**
+     * Bulma loading class for the given tracker.
+     *
+     * @param trackerName
+     */
+    loadingClass(trackerName: string = defaultKey) {
         return {
-            loading: {
-                global: false,
-            }
-        }
-    },
-
-    methods: {
-        /**
-         * Checks if the given tracker is loading.
-         *
-         * @param trackerName
-         */
-        isLoading(trackerName: string = defaultKey) {
-            return this.loading[trackerName];
-        },
-
-        /**
-         * Starts loading for the given tracker.
-         *
-         * @param trackerName
-         */
-        startLoading(trackerName: string = defaultKey) {
-            return this.$set(this.loading, trackerName, true);
-        },
-
-        /**
-         * Stops the loading state for the given tracker.
-         *
-         * @param trackerName
-         */
-        stopLoading(trackerName: string = defaultKey) {
-            return this.$set(this.loading, trackerName, false);
-        },
-
-        /**
-         * Bulma loading class for the given tracker.
-         *
-         * @param trackerName
-         */
-        loadingClass(trackerName: string = defaultKey) {
-            return {
-                'is-loading': this.isLoading(trackerName),
-            }
+            'is-loading': this.isLoading(trackerName),
         }
     }
 
-});
+}
