953b7bc5c67568423b2582db22e16f83c13c75e3
Jørgen Vatle <Jorgen@WcAServices.net>
2023-08-10 01:11:13 +0300

    Improve type inference of LoadingTracker instance.
    This also helps work around some reactivity issues in Vue 2.7

--- src/composable.ts
+++ src/composable.ts
@@ -2,33 +2,33 @@ import { reactive, set } from 'vue';
 
 type LoadingStore = Record<string, boolean>;
 
-export class LoadingTracker {
+export class LoadingTracker<StoreDefaults extends LoadingStore> {
     constructor(
         /**
          * Reactive store for the current loading tracker instance.
          */
-        public readonly store: LoadingStore
+        public readonly store: StoreDefaults
     ) {}
     
-    public isLoading(trackerName: string) {
+    public isLoading(trackerName: keyof StoreDefaults) {
         return this.store[trackerName];
     }
     
-    public start(trackerName: string) {
-        set(this.store, trackerName, true);
+    public start(trackerName: keyof StoreDefaults) {
+        set(this.store, trackerName as string, true);
     }
     
-    public stop(trackerName: string) {
-        set(this.store, trackerName, false);
+    public stop(trackerName: keyof StoreDefaults) {
+        set(this.store, trackerName as string, false);
     }
     
-    public class(trackerName: string) {
+    public class(trackerName: keyof StoreDefaults) {
         return {
             'is-loading': this.isLoading(trackerName),
         }
     }
 }
 
-export default function useLoadingTracker() {
-    return new LoadingTracker(reactive({}));
+export default function useLoadingTracker<StoreDefaults extends LoadingStore>(defaultValues: StoreDefaults) {
+    return new LoadingTracker(reactive(defaultValues));
 }
